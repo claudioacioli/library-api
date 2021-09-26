f2a2af1fb83d219b191f6447c4bf3243f3ce646d
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-26 07:58:22 -0300

    chore: Init project

---
+++ src/test/java/com/bootcamp/library/api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.library.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
