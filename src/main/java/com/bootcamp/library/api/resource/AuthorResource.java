03d1f698c8a36d901682644e473f999be4b0e594
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-26 11:28:42 -0300

    refactor: Change encapsulation

--- src/main/java/com/bootcamp/library/api/resource/AuthorResource.java
+++ src/main/java/com/bootcamp/library/api/resource/AuthorResource.java
@@ -1,5 +1,6 @@
 package com.bootcamp.library.api.resource;
 
+import com.bootcamp.library.api.dto.FormAuthorDTO;
 import com.bootcamp.library.api.dto.SimpleAuthorDTO;
 import com.bootcamp.library.api.model.Author;
 import com.bootcamp.library.api.service.AuthorService;
@@ -8,20 +9,14 @@ import org.modelmapper.ModelMapper;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-/** This service encapsulate a Simple DTO to handle author
- *
- * @author claudio
- * @see AuthorService
- * create on 2021/09/26
- */
-public class SimpleAuthorResource {
-    private static final SimpleAuthorResource instance = new SimpleAuthorResource();
-    private SimpleAuthorResource() {}
+public class AuthorResource {
+    private static final AuthorResource instance = new AuthorResource();
+    private AuthorResource() {}
 
     private final AuthorService authorService = AuthorService.getInstance();
     private final ModelMapper modelMapper = new ModelMapper();
 
-    public static SimpleAuthorResource getInstance() {
+    public static AuthorResource getInstance() {
         return instance;
     }
 
@@ -32,16 +27,15 @@ public class SimpleAuthorResource {
                 .collect(Collectors.toList());
     }
 
-    private SimpleAuthorDTO toSimpleAuthor (Author author) {
-        return modelMapper.map(author, SimpleAuthorDTO.class);
-    }
-
-    public void addAuthor (SimpleAuthorDTO author) {
-        authorService.addAuthor(author.getName(), author.getEmail(), null, author.getBirthday());
+    public void addAuthor (FormAuthorDTO author) {
+        authorService.addAuthor(author.getName(), author.getEmail(), author.getResume(), author.getBirthday());
     }
 
     public SimpleAuthorDTO getAuthor (String email) {
         return toSimpleAuthor(authorService.getAuthor(email));
     }
 
+    private SimpleAuthorDTO toSimpleAuthor (Author author) {
+        return modelMapper.map(author, SimpleAuthorDTO.class);
+    }
 }
---
+++ src/main/java/com/bootcamp/library/api/resource/AuthorResourceTester.java
@@ -0,0 +1,28 @@
+package com.bootcamp.library.api.resource;
+
+import com.bootcamp.library.api.dto.FormAuthorDTO;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/** This is service test driver
+ *
+ * @author claudio
+ * @see AuthorResource
+ * create on 2021/09/26
+ */
+public class AuthorResourceTester {
+    public static void main(String[] args) {
+        AuthorResource authorResource = AuthorResource.getInstance();
+        System.out.println(authorResource.getAuthor("calnewport@gmail.com"));
+        authorResource.addAuthor(
+                new FormAuthorDTO(
+                        "Gustavo Cerbasi",
+                        "gustavo@gmail.com",
+                        null,
+                        LocalDate.parse("1974/04/24", DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+                )
+        );
+        authorResource.getAll().forEach(System.out::println);
+    }
+}
