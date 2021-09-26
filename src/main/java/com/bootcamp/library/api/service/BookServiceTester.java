49033683d2d54fe745d19153390beafdf2739d1b
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-26 12:55:21 -0300

    test: Create test driver class

---
+++ src/main/java/com/bootcamp/library/api/service/BookServiceTester.java
@@ -0,0 +1,29 @@
+package com.bootcamp.library.api.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+/** This is service test driver
+ *
+ * @author claudio
+ * create on 2021/09/26
+ * @see BookService
+ * @see AuthorService
+ */
+public class BookServiceTester {
+    public static void main(String[] args) {
+        BookService bookService = BookService.getInstance();
+        AuthorService authorService = AuthorService.getInstance();
+
+        bookService.addBook(
+            "1-4391-6734-6",
+            "How to Win Friends and Influence People ",
+            LocalDate.parse("10/24/1936", DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+            291,
+            authorService.getAuthor("dale_carnegie@gmail.com")
+        );
+
+        bookService.getAll().forEach(System.out::println);
+    }
+}
