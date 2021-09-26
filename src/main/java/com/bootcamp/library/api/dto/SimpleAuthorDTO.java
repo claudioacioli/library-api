10c901f3154a9f6ec47e16fb5130d571b74dff3d
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-26 10:10:23 -0300

    feat: Create a simple dto to make an interface to ours clients

---
+++ src/main/java/com/bootcamp/library/api/dto/SimpleAuthorDTO.java
@@ -0,0 +1,22 @@
+package com.bootcamp.library.api.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+/** This describes a simple Author interface to our clients
+ *
+ * @author claudio
+ * create on 2021/09/36
+ * @see com.bootcamp.library.api.model.Author
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleAuthorDTO {
+    private String name;
+    private String email;
+    private LocalDate birthday;
+}
