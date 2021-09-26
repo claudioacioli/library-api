3ae2b82c5e510abf4680ef775fb66885a5d7d61a
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-09-26 12:34:32 -0300

    feat: Return a list of books

--- src/main/java/com/bootcamp/library/api/service/BookService.java
+++ src/main/java/com/bootcamp/library/api/service/BookService.java
@@ -1,10 +1,22 @@
 package com.bootcamp.library.api.service;
 
+import com.bootcamp.library.api.model.Book;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookService {
     private static final BookService instance = new BookService();
     private BookService () {}
 
+    private final Map<String, Book> mapOfBooks = new HashMap<>();
+
     public static BookService getInstance() {
         return instance;
+    }
+
+    public Collection<Book> getAll () {
+        return mapOfBooks.values();
     }
 }
