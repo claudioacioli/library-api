51fbd95472a4d43a2f55f686f57fe9e89c91ef84
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-10-10 05:43:37 -0300

    feat: Make model class a peristent entity

--- src/main/java/com/bootcamp/library/api/model/Author.java
+++ src/main/java/com/bootcamp/library/api/model/Author.java
@@ -2,6 +2,10 @@ package com.bootcamp.library.api.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 /** This describes an author of books
@@ -15,9 +19,20 @@ import java.time.LocalDate;
 @ToString(exclude = "resume")
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String email;
     private String resume;
     private LocalDate birthday;
+
+    public Author (String name, String email, String resume, LocalDate birthday) {
+        this.name = name;
+        this.email = email;
+        this.resume = resume;
+        this.birthday = birthday;
+    }
 }
