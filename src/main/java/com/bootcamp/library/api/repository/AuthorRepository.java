571aca0be81d3a061a728b5d03bfadbd82b83715
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-10-10 05:53:31 -0300

    docs: Correct date

--- src/main/java/com/bootcamp/library/api/repository/AuthorRepository.java
+++ src/main/java/com/bootcamp/library/api/repository/AuthorRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /** Manage data manipulation of authors.
  *
  * @author claudio
- * create on 2021/09/26
+ * create on 2021/10/10
  * @see Author
  */
 public interface AuthorRepository extends JpaRepository<Author, Long> {
