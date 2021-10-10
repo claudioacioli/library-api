9096ea31a977247fcabe865f3e87878dd303eb9f
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-10-10 07:19:15 -0300

    refactor: Return 201 status code and location when create new resource

--- src/main/java/com/bootcamp/library/api/controller/AuthorController.java
+++ src/main/java/com/bootcamp/library/api/controller/AuthorController.java
@@ -4,11 +4,14 @@ import com.bootcamp.library.api.dto.FormAuthorDTO;
 import com.bootcamp.library.api.dto.SimpleAuthorDTO;
 import com.bootcamp.library.api.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
+import java.net.URI;
 import java.util.Collection;
 
 /** This controller provides endpoints handle with Author entity
@@ -36,8 +39,10 @@ public class AuthorController {
     }
 
     @PostMapping
-    public void create (@RequestBody @Valid FormAuthorDTO author) {
+    public ResponseEntity create (@RequestBody @Valid FormAuthorDTO author, UriComponentsBuilder uriBuilder) {
         authorService.addAuthor(author);
+        URI uri = uriBuilder.path("/authors/{email}").buildAndExpand(author.getEmail()).toUri();
+        return ResponseEntity.created(uri).body(author);
     }
 
     @PutMapping
