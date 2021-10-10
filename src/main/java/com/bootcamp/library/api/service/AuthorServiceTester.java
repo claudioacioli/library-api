9748c90fa9f3175ede6ea5e39f4fa853b2a6f332
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-10-10 05:57:20 -0300

    refactor: Make adaptations tester class

--- src/main/java/com/bootcamp/library/api/service/AuthorServiceTester.java
+++ src/main/java/com/bootcamp/library/api/service/AuthorServiceTester.java
@@ -1,5 +1,7 @@
 package com.bootcamp.library.api.service;
 
+import com.bootcamp.library.api.dto.FormAuthorDTO;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -14,21 +16,25 @@ public class AuthorServiceTester {
         AuthorService authorService = AuthorService.getInstance();
 
         authorService.addAuthor(
+                new FormAuthorDTO(
                 "Dale Carnegie",
                 "dale_carnegie@gmail.com",
                 "Dale Carnegie (born 1988)  was an American writer and lecturer, " +
                         "and the developer of courses in self-improvement, " +
                         "salesmanship, corporate training, public speaking, and interpersonal skills.",
                 LocalDate.parse("11/24/1988", DateTimeFormatter.ofPattern("MM/dd/yyyy"))
+                )
         );
 
         authorService.addAuthor(
+                new FormAuthorDTO(
                 "Cal Newport",
                 "calnewport@gmail.com",
                 "Cal Newport (born 1982/06/23)" +
                         " is an American non-fiction author and associate professor " +
                         "of computer science at Georgetown University.",
                 LocalDate.parse("1982/06/23", DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+                )
         );
 
         authorService.getAll().forEach(System.out::println);
