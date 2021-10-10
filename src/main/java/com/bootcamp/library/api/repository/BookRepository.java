3e2edb6c5037f15f636e03ae1371fcf0f1a87cf6
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-10-10 08:44:31 -0300

    docs: Add some docs

--- src/main/java/com/bootcamp/library/api/repository/BookRepository.java
+++ src/main/java/com/bootcamp/library/api/repository/BookRepository.java
@@ -3,5 +3,11 @@ package com.bootcamp.library.api.repository;
 import com.bootcamp.library.api.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/** Manage data manipulation of books.
+ *
+ * @author claudio
+ * create on 2021/10/10
+ * @see Book
+ */
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
